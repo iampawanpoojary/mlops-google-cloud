c67cc6ab5ca5ee4b27c311dc2f4fdfa198bc415a
iampawanpoojary <iampawanpoojary@gmail.com>
2021-10-28 00:54:32 +0200

    fix: Minor fixes

--- run_pipeline.py
+++ run_pipeline.py
@@ -4,4 +4,4 @@ api_client = AIPlatformClient(project_id='389886591986',
                               region='us-central1')
 
 pipeline_run_name = api_client.create_run_from_job_spec(
-    job_spec_path='gs://pawan-vertex-demo/test-pipeine')
+    job_spec_path='gs://pawan-vertex-demo/test_pipeine')
