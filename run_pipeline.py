930a74cb8db8a4da64b84d21af0cc1ae55d1a25d
iampawanpoojary <iampawanpoojary@gmail.com>
2021-10-28 00:51:59 +0200

    feat: add pipeline runner

---
+++ run_pipeline.py
@@ -0,0 +1,7 @@
+from kfp.v2.google.client import AIPlatformClient
+
+api_client = AIPlatformClient(project_id='389886591986',
+                              region='us-central1')
+
+pipeline_run_name = api_client.create_run_from_job_spec(
+    job_spec_path='gs://pawan-vertex-demo/test-pipeine')
