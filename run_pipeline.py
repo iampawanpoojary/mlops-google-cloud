45aa6567d032a792d9bcd6bdb39af59992516336
iampawanpoojary <iampawanpoojary@gmail.com>
2021-10-28 01:53:21 +0200

    feat: add pipeline root

--- run_pipeline.py
+++ run_pipeline.py
@@ -4,5 +4,6 @@ api_client = AIPlatformClient(project_id='389886591986',
                               region='us-central1')
 
 pipeline_run_name = api_client.create_run_from_job_spec(
-    job_spec_path='gs://pawan-vertex-demo/test_pipeline.json')
+    job_spec_path='gs://pawan-vertex-demo/test_pipeline.json',
+    pipeline_root='gs://pawan-vertex-demo/test_pipeline')
 
