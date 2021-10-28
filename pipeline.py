392ab61fd446151af502789385164acfe2f22f98
iampawanpoojary <iampawanpoojary@gmail.com>
2021-10-28 02:28:19 +0200

    feat: add automl job

--- pipeline.py
+++ pipeline.py
@@ -1,28 +1,43 @@
 import kfp
-
-from typing import NamedTuple
-
-from kfp.v2.dsl import pipeline
-from kfp.v2.dsl import component
 from kfp.v2 import compiler
+from kfp.v2.google.client import AIPlatformClient
+from google.cloud import aiplatform
+from google_cloud_pipeline_components import aiplatform as gcc_aip
 
-@component() 
-def concat(a: str, b: str) -> str:
-  return a + b
+project_id = 'mlop-cg-data-and-insights'
+region = 'us-central1'
+pipeline_root_path = 'gs://pawan-vertex-demo/test_pipeline'
 
-@component
-def reverse(a: str)->NamedTuple("outputs", [("before", str), ("after", str)]):
-  return a, a[::-1]
-
-@pipeline(name="test-pipeline",
-description="CICD test", 
-pipeline_root='gs://pawan-vertex-demo/test_pipeline'
-             )
-def basic_pipeline(a: str='stres', b: str='sed'):
-    concat_task = concat(a, b)
-    reverse_task = reverse(concat_task.output)
-
-if __name__ == '__main__':
-    compiler.Compiler().compile(
-        pipeline_func=basic_pipeline, package_path="test_pipeline.json"
+@kfp.dsl.pipeline(
+    name="automl-image-training",
+    pipeline_root=pipeline_root_path)
+def pipeline(project_id: str):
+    ds_op = gcc_aip.ImageDatasetCreateOp(
+        project=project_id,
+        display_name="flowers",
+        gcs_source="gs://cloud-samples-data/vision/automl_classification/flowers/all_data_v2.csv",
+        import_schema_uri=aiplatform.schema.dataset.ioformat.image.single_label_classification,
     )
+
+    training_job_run_op = gcc_aip.AutoMLImageTrainingJobRunOp(
+        project=project_id,
+        display_name="train-iris-automl-mbsdk-1",
+        prediction_type="classification",
+        model_type="CLOUD",
+        base_model=None,
+        dataset=ds_op.outputs["dataset"],
+        model_display_name="iris-classification-model-mbsdk",
+        training_fraction_split=0.6,
+        validation_fraction_split=0.2,
+        test_fraction_split=0.2,
+        budget_milli_node_hours=8000,
+    )
+
+    endpoint_op = gcc_aip.ModelDeployOp(
+        project=project_id, model=training_job_run_op.outputs["model"]
+    )
+
+
+compiler.Compiler().compile(pipeline_func=pipeline,
+        package_path='image_classification_pipeline.json')
+
--- run_pipeline.py
+++ run_pipeline.py
@@ -1,9 +1,15 @@
 from kfp.v2.google.client import AIPlatformClient
 
-api_client = AIPlatformClient(project_id='389886591986',
-                              region='us-central1')
 
-pipeline_run_name = api_client.create_run_from_job_spec(
-    job_spec_path='gs://pawan-vertex-demo/test_pipeline.json',
-    pipeline_root='gs://pawan-vertex-demo/test_pipeline')
+project_id = 'mlop-cg-data-and-insights'
+region = 'us-central1'
+pipeline_root_path = 'gs://pawan-vertex-demo/test_pipeline'
 
+api_client = AIPlatformClient(project_id=project_id, region=region)
+
+response = api_client.create_run_from_job_spec(
+    'image_classification_pipeline.json',
+    pipeline_root=pipeline_root_path,
+    parameter_values={
+        'project_id': project_id
+    })
